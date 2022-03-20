0a92782967fa274fb899ecb13b3827f7ce4b76fc
piyar12Nov <99732046+piyar12Nov@users.noreply.github.com>
2022-03-20 09:53:02 -0700

    Add files via upload

--- dino run!/piyar12Nov.github.io/dino run!/script.js
+++ dino run!/piyar12Nov.github.io/dino run!/script.js
@@ -3,28 +3,19 @@ function jump(){
     dino.classList.add("animate");
 }
 function jump() {
-    if(dino.classList != "animate"){
-        dino.classList.add("animate");
-    }
-    setTimeout(function() {
-        dino.classList.remove("animate");
-    },500);
+if(dino.classList != "animate"){
+    dino.classList.add("animate");
 }
-
-var score = 0;
-var dead = false;
+setTimeout(function() {
+    dino.classList.remove("animate");
+},500);
+}
 var checkDead = setInterval (function() {
     var dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("Left"));
-    
     if(blockLeft<20 && blockLeft>0 && dinoTop>=130) {
         block.style.animation ="none";
         block.style.display="none";
-        console.log("you're dead");
-        dead = true;
-    }else if(dinoTop == 150 && dead == false){
-        score = ++score;
-        console.log("get score " + score);
-        document.getElementById("score").innerHTML = "Score  : " + score
+        alert("you're dead");
     }
 },10);
